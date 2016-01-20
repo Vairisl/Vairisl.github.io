26f9316cc00bd74aa6699dc3f7b6bd18c6b1056a
vairisl <vairiks123@gmai.com>
2016-01-20 10:42:39 +0200

    Update

--- index-reg.php
+++ index-reg.php
@@ -1,3 +1,6 @@
+<?php
+include ('class/base.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -28,57 +31,10 @@
 	<!-- HEAD BEIGAS -->
 
 	<body>
-		<!-- BODY SĀKUMS -->
-		<nav class="navbar navbar-inverse" role="navigation">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="index.php"><i class="fa fa-home"></i></a>
-				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li>
-							<a href="#"><i class="fa fa-phone"></i> Kontakti</a>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-picture-o"></i> Galerija <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li role="separator" class="divider"></li>
-								<li>
-									<a href="rock.php"><i class="fa fa-hand-rock-o"></i> Rock</a>
-								</li>
-								<li role="separator" class="divider"></li>
-								<li>
-									<a href="metal.php"><i class="fa fa-star-o"></i> Power Metal</a>
-								</li>
-								<li role="separator" class="divider"></li>
-								<li>
-									<a href="numetal.php"><i class="fa fa-star"></i> Nu Metal</a>
-								</li>
-								<li role="separator" class="divider"></li>
-							</ul>
-						</li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li>
-							<a href="register.php"><i class="fa fa-users"></i> Reģistrēties</a>
-						</li>
-						<li>
-							<a href="login.php"><i class="fa fa-user-secret"></i> Ielogoties</a>
-						</li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container-fluid -->
-		</nav>
-
+    <?php
+    $template = new templateClass();
+    $template->getLogMenu();
+    ?>
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-12 blog-main">
--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+include ('class/base.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
